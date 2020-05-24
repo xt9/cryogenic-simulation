a99ebf8d1c1ccc0109d84bd988f00d671afa5d8c
xt9 <niklas_andersson90@hotmail.com>
2020-05-24 19:57:07 +0200

    Removed twitch exporter

--- build.bat
+++
@@ -1,7 +0,0 @@
-set PACKNAME=cryogenic_simulation
-set VERSION=0.4.0
-set FILENAME=%PACKNAME%-%VERSION%.zip
-set BUILD_JSON=client-build.json
-
-twitch_export.exe -p %VERSION% -n %PACKNAME% -c %BUILD_JSON%
-move %FILENAME% build/
